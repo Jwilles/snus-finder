f6e7a74188721672ac24b627b714cf554e35015c
root <root@mean-512mb-tor1-01.localdomain>
2017-01-30 04:34:49 +0000

    updated for deployment

--- README.md
+++ README.md
@@ -8,6 +8,3 @@ After install simply run the app with "npm start" from the root directory
 
 ## API Key
 AIzaSyD7dR3nH2OZBdFrwmU6AFLQCGttEwhaxjY
-
-
-apiKey = YOUR KEY HERE
--- app/snus-nightmare.js
+++ app/snus-nightmare.js
@@ -6,17 +6,18 @@ var Nightmare = require('nightmare');
 
 module.exports = function (loc) {
 
-	testLoc = {
-		lat: 34.08,
-		lon: -118.44
-		}
+//	testLoc = {
+//		lat: 34.08,
+//		lon: -118.44
+//		}
 
 	return new Promise((resolve, reject) => {
 		var nightmare = new Nightmare({show: false});
 		
 		nightmare
 			.goto('https://generalsnus.com/m/find')
-			.type('form[action*="/m/find"] [name = location]', testLoc.lat + ' ' + testLoc.lon)
+			//.type('form[action*="/m/find"] [name = location]', testLoc.lat + ' ' + testLoc.lon)
+			.type('form[action*="/m/find"] [name = location]', loc.lat + ' ' + loc.lon)
 			.click('form[action*="/m/find"] [id=btnMapIt]')
   			.wait(5000)
   			.evaluate(function () {
